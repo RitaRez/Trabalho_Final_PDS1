1b9633d487b3fb3f3ecf00c510388563712cb41d
Luiz Philippe <luiz@phari.solutions>
2019-06-10 11:39:37 -0300

    Correção no resultado

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 
 int main(int argc, char *argv[]){
 
-    Tabela tabela = ler_arquivo(argv[1]);  
-    
+    Tabela tabela = ler_arquivo(argv[1]);
+
     for(int i = 1; i < tabela.qtdSequencias; i++){
         tabela = lcs(tabela, i);
-    }         
-        
+    }
+
 
     tabela.celulas[0].semelhanca = 0;
     tabela.celulas[0].alinhamento1 = "-";
@@ -16,7 +16,7 @@ int main(int argc, char *argv[]){
 
     ordena_tabela(tabela.celulas, 0, tabela.qtdSequencias - 1);
 
-    imprime_celula(tabela.celulas[tabela.qtdSequencias - 1]);
+    imprime_celula(tabela.celulas[0]);
     imprime_tabela(tabela);
 
     free(tabela.celulas);
